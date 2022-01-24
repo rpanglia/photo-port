761de40fade0cb31aec0b18a1cdfb559d02d2c01
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-24 15:35:58 -0500

    about and nav updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-import logo from './logo.svg';
-import './App.css';
+
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
